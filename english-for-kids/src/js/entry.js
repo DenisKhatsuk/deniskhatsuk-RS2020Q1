8898c35e1857fb33b4c5fb44b93dd165362d692a
Denis Khatsuk <dennis.hatsuk@gmail.com>
2020-04-18 23:50:51 +0300

    feat: add Material Design for Bootstrap. set styles packing

--- english-for-kids/src/js/entry.js
+++ english-for-kids/src/js/entry.js
@@ -1,1 +1,5 @@
+import '../sass/style.scss'
+import '../vendor/mdb/css/bootstrap.min.css'
+import '../vendor/mdb/css/mdb.min.css'
+
 console.log('Start');
--- english-for-kids/webpack.config.js
+++ english-for-kids/webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env, options) => {
   const isProduction = options.mode === 'production';
@@ -26,10 +27,30 @@ module.exports = (env, options) => {
             }
           }
         },
+        {
+          test: /\.s?css$/,
+          exclude: /node_modules/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|svg|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        },
       ]
     },
     plugins: [
       new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: 'styles.css',
+      }),
     ],
   }
 
