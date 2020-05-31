bda56f751c84ecf040d1d29aeb3956404c6d9c86
Denis Khatsuk <dennis.hatsuk@gmail.com>
2020-05-31 21:17:23 +0300

    feat: add DateHandler

---
+++ fancy-weather/src/js/components/DateHandler.js
@@ -0,0 +1,16 @@
+function getCurrentDate(language = 'en-US') {
+  const DATE = new Date();
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  };
+  return DATE.toLocaleString(language, options);
+}
+
+export default {
+  getCurrentDate,
+};
