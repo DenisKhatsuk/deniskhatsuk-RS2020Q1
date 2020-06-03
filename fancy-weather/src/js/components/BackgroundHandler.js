226e0db6dd4a4394e2a565cdd0b8640f031eccc4
Denis Khatsuk <dennis.hatsuk@gmail.com>
2020-06-04 00:26:43 +0300

    fix: remove typo from BackgroundHandler

--- fancy-weather/src/js/components/BackgroundHandler.js
+++ fancy-weather/src/js/components/BackgroundHandler.js
@@ -1,5 +1,5 @@
 const API_KEY = 'tU1UjIRY5qx9r1GfNJTqTzaYEeFHY5iY1uHvzPycTLE';
-const REQUEST_URL = `https://api.unsplash.com/photos/random?orientation=landscape&per_page=1&query=тфегку+summer&client_id=${API_KEY}`;
+const REQUEST_URL = `https://api.unsplash.com/photos/random?orientation=landscape&per_page=1&query=nature+summer&client_id=${API_KEY}`;
 
 async function getImageURL() {
   const response = await fetch(REQUEST_URL);
