6c8cbc0a1c72514a5e170a994532d7121b78d750
Denis Khatsuk <dennis.hatsuk@gmail.com>
2020-06-08 23:11:10 +0300

    refactor: fix eslint errors in ContentBuilder

--- movie-search/src/js/components/ContentBuilder.js
+++ movie-search/src/js/components/ContentBuilder.js
@@ -1,14 +1,13 @@
 export default class ContentBuilder {
-
   constructor(parentClass, ...blocks) {
     this.target = document.querySelector(`.${parentClass}`);
     this.fragment = document.createDocumentFragment();
-    blocks.forEach((el)=>{
+    blocks.forEach((el) => {
       this.fragment.appendChild(el);
     });
   }
 
   addContentToDOM() {
     this.target.prepend(this.fragment);
-  };
+  }
 }
