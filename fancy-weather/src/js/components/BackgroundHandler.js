01d8cef9da5135b40a079244a856e38771223dcd
Denis Khatsuk <dennis.hatsuk@gmail.com>
2020-06-09 20:55:45 +0300

    refactor: replace default background image from online to be local

--- fancy-weather/src/js/components/BackgroundHandler.js
+++ fancy-weather/src/js/components/BackgroundHandler.js
@@ -1,5 +1,7 @@
 // https://unsplash.com/documentation
 
+import Image from '../../img/background/default.jpg';
+
 const API_KEY = '4EHFU1PJFteXWJSH7FYI_K2SIpSD4_u7kSG2ddVq_Z8';
 const REQUEST_URL = `https://api.unsplash.com/photos/random?orientation=landscape&per_page=1&query=nature+summer&client_id=${API_KEY}`;
 
@@ -18,7 +20,7 @@ async function setBackgroundImage() {
       document.body.style.backgroundImage = `linear-gradient(0deg, rgba(0,0,0,1) 7%, rgba(0,0,0,0.4) 27%), url(${backgroundImage.src})`;
     });
   } catch (error) {
-    document.body.style.backgroundImage = 'linear-gradient(0deg, rgba(0,0,0,1) 7%, rgba(0,0,0,0.4) 27%), url("https://images.unsplash.com/photo-1421284621639-884f4129b61d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80")';
+    document.body.style.backgroundImage = `linear-gradient(0deg, rgba(0,0,0,1) 7%, rgba(0,0,0,0.4) 27%), url(${Image})`;
   }
 }
 
