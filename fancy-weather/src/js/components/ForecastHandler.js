6375b8245d9a6777b22ca3a7aca8583854a172aa
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2020-09-17 19:01:02 +0300

    feat: add error handling to ForecastHandler

--- fancy-weather/src/js/components/ForecastHandler.js
+++ fancy-weather/src/js/components/ForecastHandler.js
@@ -19,7 +19,7 @@
 
 import Date from './DateHandler';
 
-const API_KEY = '5bc699ca9036e60c3f7d417600991566';
+const API_KEY = '5bc699ca9036e60c3f7d4176009915666';
 
 class ForecastHandler {
   constructor() {
@@ -28,6 +28,7 @@ class ForecastHandler {
     this.lang = 'en';
     this.units = 'metric';
     this.weather = {};
+    this.errorMessage = 'Something went wrong during forecast request. Please try again later';
   }
 
   static addForecastContainer(parent) {
@@ -52,69 +53,79 @@ class ForecastHandler {
 
   publishTodayWeather(parentSelector = 'body') {
     const todayWeatherContainer = document.querySelector(parentSelector);
-    const {
-      temp,
-      feels_like, /* eslint-disable-line */
-      humidity,
-      wind_speed, /* eslint-disable-line */
-      description,
-      icon,
-    } = this.weather.todayWeather;
-    const temperature = Math.round(temp);
-    const feels = Math.round(feels_like);
-    const wind = Math.round(wind_speed);
+    if (this.weather.todayWeather) {
+      const {
+        temp,
+        feels_like, /* eslint-disable-line */
+        humidity,
+        wind_speed, /* eslint-disable-line */
+        description,
+        icon,
+      } = this.weather.todayWeather;
+      const temperature = Math.round(temp);
+      const feels = Math.round(feels_like);
+      const wind = Math.round(wind_speed);
 
-    todayWeatherContainer.innerHTML = `
-      <div class="forecast__today_temp">${temperature}°</div>
-      <div class="forecast__today_details">
-        <div class="forecast__today_icon" style="background-image: url('http://openweathermap.org/img/wn/${icon}@4x.png')"></div>
-        <div>${description}</div>
-        <div>Feels like: ${feels}°C</div>
-        <div>Wind: ${wind} m/s</div>
-        <div>Humidity: ${humidity}%</div>
-      </div>
-      `;
+      todayWeatherContainer.innerHTML = `
+        <div class="forecast__today_temp">${temperature}°</div>
+        <div class="forecast__today_details">
+          <div class="forecast__today_icon" style="background-image: url('http://openweathermap.org/img/wn/${icon}@4x.png')"></div>
+          <div>${description}</div>
+          <div>Feels like: ${feels}°C</div>
+          <div>Wind: ${wind} m/s</div>
+          <div>Humidity: ${humidity}%</div>
+        </div>
+        `;
+    } else {
+      todayWeatherContainer.innerHTML = `
+        <div class="forecast__error">
+          ${this.errorMessage}
+        </div>
+        `;
+    }
     return this;
   }
 
   publishForecast(parentSelector = 'body') {
     const forecastContainer = document.querySelector(parentSelector);
-    const forecastFragment = new DocumentFragment();
-    this.weather.forecast.splice(3);
-    this.weather.forecast.forEach((dayWeather) => {
-      const {
-        dt: dateUnix,
-        temp: {
-          day: tempDay,
-          night: tempNight,
-        },
-        humidity,
-        weather: [
-          {
-            icon,
+    if (this.weather.forecast) {
+      const forecastFragment = new DocumentFragment();
+      this.weather.forecast.splice(3);
+      this.weather.forecast.forEach((dayWeather) => {
+        const {
+          dt: dateUnix,
+          temp: {
+            day: tempDay,
+            night: tempNight,
           },
-        ],
-      } = dayWeather;
-      const day = Date.getShortDate(dateUnix);
-      const temperatureDay = Math.round(tempDay);
-      const temperatureNight = Math.round(tempNight);
-      const dayStructure = document.createElement('div');
-      dayStructure.classList.add('forecast__upcoming_day');
-      dayStructure.innerHTML = `
-        <div class="forecast__upcoming_header">
-          <div>${day}</div>
-          <div class="forecast__upcoming_icon" style="background-image: url('http://openweathermap.org/img/wn/${icon}@2x.png')"></div>
-        </div>
-        <div class="forecast__upcoming_main">
-          <div class="forecast__upcoming_temp-day">${temperatureDay}°</div>
-          <div class="forecast__upcoming_temp-night">${temperatureNight}°</div>
-          <div class="forecast__upcoming_humidity">${humidity}%</div>
-        </div>
-      `;
-      forecastFragment.appendChild(dayStructure);
-    });
-    forecastContainer.innerHTML = '';
-    forecastContainer.appendChild(forecastFragment);
+          humidity,
+          weather: [
+            {
+              icon,
+            },
+          ],
+        } = dayWeather;
+        const day = Date.getShortDate(dateUnix);
+        const temperatureDay = Math.round(tempDay);
+        const temperatureNight = Math.round(tempNight);
+        const dayStructure = document.createElement('div');
+        dayStructure.classList.add('forecast__upcoming_day');
+        dayStructure.innerHTML = `
+          <div class="forecast__upcoming_header">
+            <div>${day}</div>
+            <div class="forecast__upcoming_icon" style="background-image: url('http://openweathermap.org/img/wn/${icon}@2x.png')"></div>
+          </div>
+          <div class="forecast__upcoming_main">
+            <div class="forecast__upcoming_temp-day">${temperatureDay}°</div>
+            <div class="forecast__upcoming_temp-night">${temperatureNight}°</div>
+            <div class="forecast__upcoming_humidity">${humidity}%</div>
+          </div>
+        `;
+        forecastFragment.appendChild(dayStructure);
+      });
+      forecastContainer.innerHTML = '';
+      forecastContainer.appendChild(forecastFragment);
+    }
     return this;
   }
 
@@ -130,40 +141,50 @@ class ForecastHandler {
     this.lang = lang || this.lang;
     this.units = units || this.units;
     const REQUEST_URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${this.lat}&lon=${this.long}&lang=${this.lang}&units=${this.units}&APPID=${API_KEY}`;
-    const request = await fetch(REQUEST_URL);
-    const {
-      lat: latitude,
-      lon: longitude,
-      current: {
-        dt: date,
-        temp,
-        feels_like, /* eslint-disable-line */
-        humidity,
-        wind_speed, /* eslint-disable-line */
-        weather: [
+    try {
+      const request = await fetch(REQUEST_URL);
+      if (!request.ok) {
+        throw new Error('Bad response from server. Weather server connection failed.');
+      } else {
+        const {
+          lat: latitude,
+          lon: longitude,
+          current: {
+            dt: date,
+            temp,
+            feels_like, /* eslint-disable-line */
+            humidity,
+            wind_speed, /* eslint-disable-line */
+            weather: [
+              {
+                description,
+                icon,
+              },
+            ],
+          },
+          daily,
+        } = await request.json();
+        this.weather = {
+          geolocation: { latitude, longitude },
+          todayWeather:
           {
+            date,
+            temp,
+            feels_like,
+            humidity,
+            wind_speed,
             description,
             icon,
           },
-        ],
-      },
-      daily,
-    } = await request.json();
-    this.weather = {
-      geolocation: { latitude, longitude },
-      todayWeather:
-      {
-        date,
-        temp,
-        feels_like,
-        humidity,
-        wind_speed,
-        description,
-        icon,
-      },
-      forecast: daily,
-    };
-    return this.weather;
+          forecast: daily,
+        };
+        return this.weather;
+      }
+    } catch (error) {
+      /* eslint no-console: ["error", { allow: ["warn"] }] */
+      console.warn(error.message);
+      return false;
+    }
   }
 }
 
